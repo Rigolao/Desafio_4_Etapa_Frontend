3293d8780cc672bc4be0bb9d1b059b2b8ceb83e0
Joao Marques <joamarquescontato@gmail.com>
2022-09-27 10:33:11 -0300

    Adicionando novos estilos na tela de login

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,26 +3,60 @@ import {Box, Stack} from "@mui/material";
 import {Button, TextField} from "@mui/material";
 
 
-function Formulario (){
+function Formulario() {
 
     return (
-        <Box sx={{width: "50vw", height: "100vh", textAlign: "center", alignItems: "center", display: "flex", justifyContent: "flex-end"}}>
-            <Box sx={{width: "368px",height:"472px",margin: "0 auto"}}>
-                <Stack spacing={2}>
-                    <h1 style={{fontSize: "34px", fontWeight: "bold", textAlign: "left"}}>Faça seu login</h1>
-                    <TextField label="CPF" required variant="filled"/>
-                    <TextField label="Senha" type="password" required variant="filled"/>
+        <Box sx={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gridTemplateRows: "1fr",
+            width: "100%",
+            height: "100vh",
+            padding: "1rem"
+        }}>
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                background: "#6C63FF",
+                borderRadius: "20px",
 
-                    <Stack direction="row" justifyContent="space-between" spacing={2} >
-                        <Button sx={{backgroundColor: "#6C63FF", widht: "100px", height: "55px"}} variant="contained">Entrar</Button>
-                        <a style={{color: "#6C63FF", curso: "pointer", fontSize: "12px", lineHeight: "55px", fontWeight: "semibold"}}>Esqueci minha senha</a>
+            }}>
+
+            </Box>
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start"
+            }}>
+                <Box sx={{width: "368px", height: "472px", margin: "0 auto"}}>
+                    <Stack spacing={2}>
+                        <h1 style={{fontSize: "34px", fontWeight: "bold", textAlign: "left"}}>Faça seu login</h1>
+                        <TextField label="CPF" required variant="filled"/>
+                        <TextField label="Senha" type="password" required variant="filled"/>
+
+                        <Stack direction="row" justifyContent="space-between" spacing={2}>
+                            <Button sx={{backgroundColor: "#6C63FF", widht: "100px", height: "55px"}}
+                                    variant="contained">Entrar</Button>
+                            <a style={{
+                                color: "#6C63FF",
+                                curso: "pointer",
+                                fontSize: "12px",
+                                lineHeight: "55px",
+                                fontWeight: "semibold"
+                            }}>Esqueci minha senha</a>
+                        </Stack>
+                        <Stack spacing={6}>
+                            <p style={{fontSize: "12px", textAlign: "left"}}><strong>Não tem uma conta? </strong><a
+                                style={{color: "#6C63FF"}}>Cadastra-se</a></p>
+                        </Stack>
                     </Stack>
-                    <Stack spacing={6}>
-                        <p style={{fontSize: "12px", textAlign: "left"}}><strong>Não tem uma conta? </strong><a style={{color: "#6C63FF"}}>Cadastra-se</a></p>
-                    </Stack>
-                </Stack>
+                </Box>
             </Box>
         </Box>
-    );
+
+    )
+        ;
 }
+
 export default Formulario;
