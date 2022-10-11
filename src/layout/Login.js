84cb8004100030cf4248f504443bb220d4fdc8a4
Joao Marques <joamarquescontato@gmail.com>
2022-10-11 02:03:38 -0300

    Adicionado popup conforme a autenticação do usuario

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
-import {Box, InputAdornment, Link, Stack, styled, Typography, useTheme} from "@mui/material";
+import {
+    Alert, AlertTitle, Box, Collapse, InputAdornment, Link, Stack, styled, Typography, useTheme
+} from "@mui/material";
 import {Button, TextField} from "@mui/material";
 import Center from "../componente/Center";
 import theme from "../theme";
@@ -7,6 +9,15 @@ import useViewport from "../hooks/useViewport";
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined';
+import IconButton from "@mui/material/IconButton";
+import {Close} from "@mui/icons-material";
+import * as PropTypes from "prop-types";
+
+function CloseIcon(props) {
+    return null;
+}
+
+CloseIcon.propTypes = {fontSize: PropTypes.string};
 
 function Login(props) {
 
@@ -18,95 +29,108 @@ function Login(props) {
 
     const theme = useTheme();
 
-    const [cpf, setCpf] = useState("");
-    const [senha, setSenha] = useState("");
+    const [cpf, setCpf] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [tipoSeveridade, setTipoSeveridade] = useState('error');
+    const [tipoMsg, setTipoMsg] = useState('')
 
-    function mudaCpf (e) {
+    const handleChange = (e) => {
         setCpf(e.target.value);
-        console.log(e.target.value);
     }
-
-    function mudaSenha (e) {
+    const handleSenhaChange = (e) => {
         setSenha(e.target.value);
     }
 
-    function login(){
-        if(cpf == 45176048884 && senha == 1234){
-            return <span>Valido</span>
+    const handleSubmit = (e) => {
+        if (cpf == '45176048884' && senha == '1234') {
+            setErro(false);
+            setOpen(true);
+            setTipoMsg("Verificação aprovada!")
+            setTipoSeveridade('success');
+        } else {
+            setErro(true);
+            setOpen(true);
+            setTipoMsg("CPF ou senha incorreto!")
+            setTipoSeveridade('error');
         }
-        else{
-            return <span>Falso</span>
-        }
+        e.preventDefault();
     }
 
-    return (
-        <GridLayout>
-            { useViewport().width > 900
-                ? (
-                    <Center>
-                        <LeftColor/>
-                    </Center>
-                )
-                : <Box/>
-            }
-            <Center>
-                <Box sx={{
-                    width: "368px",
-                    height: "472px",
-                    margin: "30px"
-                }}>
-                    <form method="post">
-                        <Stack spacing={2}>
-                            <h1 style={{fontSize: "34px", fontWeight: "bold", textAlign: "left"}}>Faça seu login</h1>
-                            <TextField  label="CPF" required variant="filled" onChange={mudaCpf}/>
-                            <TextField type={showPassword ? 'text' : 'password'}
-                                       label="Senha" required variant="filled"  onChange={mudaSenha}
-                                       InputProps={{
-                                           endAdornment: (
-                                               <InputAdornment position="end" onClick={handleClick}>
-                                                   {showPassword
-                                                       ? <VisibilityOffOutlinedIcon/>
-                                                       : <RemoveRedEyeOutlinedIcon/>
-                                                   }
-                                               </InputAdornment>
-                                           ),
-                                       }}/>
-                            <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                                <Button sx={{backgroundColor: "#6C63FF", widht: "100px", height: "55px"}}
-                                        variant="contained" onSubmit={login}>Entrar</Button>
-                                <Link variant="ancora">Esqueci minha senha</Link>
-                            </Stack>
-                            <Stack spacing={6}>
-                                <Typography style={{fontSize: "12px", textAlign: "left"}}><strong>Não tem uma
-                                    conta? </strong><Link variant="ancora">Cadastra-se <ArrowRightAltOutlinedIcon/></Link></Typography>
-                            </Stack>
+
+    return (<GridLayout>
+        {useViewport().width > 900 ? (<Center>
+            <LeftColor/>
+        </Center>) : <Box/>}
+        <Center>
+            <Box sx={{
+                width: "368px", height: "472px", margin: "30px"
+            }}>
+                <form onSubmit={(e) => {
+                    handleSubmit(e)
+                }} method="post">
+                    <Stack spacing={2}>
+                        <h1 style={{fontSize: "34px", fontWeight: "bold", textAlign: "left"}}>Faça seu login</h1>
+                        <TextField label="CPF" required variant="filled" onChange={(e) => {
+                            handleChange(e)
+                        }} error={erro}/>
+                        <TextField type={showPassword ? 'text' : 'password'}
+                                   label="Senha" required variant="filled" onChange={(e) => {
+                            handleSenhaChange(e)
+                        }}
+                                   InputProps={{
+                                       endAdornment: (<InputAdornment position="end" onClick={handleClick}>
+                                           {showPassword ? <VisibilityOffOutlinedIcon/> :
+                                               <RemoveRedEyeOutlinedIcon/>}
+                                       </InputAdornment>),
+                                   }}
+                                   error={erro}
+
+                        />
+                        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+                            <Button sx={{backgroundColor: "#6C63FF", widht: "100px", height: "55px"}}
+                                    variant="contained" type="submit" value="Submit">Entrar</Button>
+                            <Link variant="ancora">Esqueci minha senha</Link>
                         </Stack>
-                    </form>
-                </Box>
-            </Center>
-        </GridLayout>
-    );
+                        <Stack spacing={6}>
+                            <Typography style={{fontSize: "12px", textAlign: "left"}}><strong>Não tem uma
+                                conta? </strong><Link
+                                variant="ancora">Cadastra-se <ArrowRightAltOutlinedIcon/></Link></Typography>
+                        </Stack>
+                        <Collapse in={open}>
+                            <Alert severity={tipoSeveridade} action={
+                                <IconButton
+                                    aria-label="close"
+                                    color="inherit"
+                                    size="small"
+                                    onClick={() => {
+                                        setOpen(false);
+                                    }}
+                                >
+                                    <Close/>
+                                </IconButton>}
+                            >
+                                {tipoMsg}
+                            </Alert>
+                        </Collapse>
+                    </Stack>
+                </form>
+            </Box>
+        </Center>
+    </GridLayout>);
 }
 
 export const GridLayout = styled(Box)({
-    display: "grid",
-    [theme.breakpoints.up('md')]: {
+    display: "grid", [theme.breakpoints.up('md')]: {
         gridTemplateColumns: "1fr 1fr",
-    },
-    [theme.breakpoints.down('md')]: {
+    }, [theme.breakpoints.down('md')]: {
         gridTemplateColumns: "1fr",
-    },
-    gridTemplateRows: "1fr",
-    width: "100%",
-    height: "100vh",
-    padding: "1rem"
+    }, gridTemplateRows: "1fr", width: "100%", height: "100vh", padding: "1rem"
 })
 
 export const LeftColor = styled(Box)({
-    backgroundColor: "#6C63FF",
-    height: "100%",
-    width: "100%",
-    borderRadius: "20px",
+    backgroundColor: "#6C63FF", height: "100%", width: "100%", borderRadius: "20px",
 })
 
 export default Login;
