58f24a98143411b931ddac030c3d4bd9ec876798
Matheus Rigolão <mrigolao@gmail.com>
2022-10-11 17:30:05 -0300

    Insercao de imagem

--- src/layout/Cadastro.js
+++ src/layout/Cadastro.js
@@ -1,61 +1,80 @@
-import React from 'react';
+import React from "react";
 import "./Tela.css";
-import {Button, TextField} from "@mui/material";
+import imagem from "../imagens/cadastro.svg";
+import Center from "../componente/Center";
+import { Button, TextField } from "@mui/material";
 
 export default function Cadastro() {
-    return (
-        <main className="gridMain">
-            <section className="section">
-                <h1>Dados pessoais</h1>
-                <h3>Preencha os campos do(a) aluno(a) que irá usar a plataforma</h3>
-                <form className="formulario">
-                    <div className="input-duplo">
-                        <TextField label="Nome" required/>
-                        <TextField label="Sobrenome" required/>
-                    </div>
-                    <div className="input-single">
-                        <TextField label="Email" style={{width: "100%"}} required/>
-                    </div>
-                    <div className="input-single">
-                        <TextField label="Confirmar email" style={{width: "100%"}} required/>
-                    </div>
-                    <div className="input-single">
-                        <TextField label="CPF" style={{width: "100%"}} required/>
-                    </div>
-                    <div className="input-single">
-                        <TextField label="Senha" style={{width: "100%"}} required/>
-                    </div>
-                    <div className="input-single">
-                        <TextField label="Confirmar senha" style={{width: "100%"}} required/>
-                    </div>
-                    <div className="input-duplo">
-                        <TextField label="Data de nascimento" required/>
-                        <TextField label="Lattes ID" required/>
-                    </div>
-                    <div className="input-single">
-                        <TextField label="E-mail alternativo" style={{width: "100%"}}/>
-                    </div>
-                    <div className="botao-row">
-                        <Button sx={{
-                            width: "138px",
-                            height: "55px"
-                        }}
-                                variant="outlined">Voltar</Button>
-                        <Button sx={{
-                            backgroundColor: "#6C63FF",
-                            width: "208px",
-                            height: "55px"
-                        }}
-                                variant="contained">Entrar</Button>
-                    </div>
-
-                </form>
-            </section>
-            <aside className="aside">
-                <div className="background">
-                    <img src="../imagens/cadastro.svg"/>
-                </div>
-            </aside>
-        </main>
-    );
+  return (
+    <main className="gridMain">
+      <section className="section">
+        <h1>Dados pessoais</h1>
+        <h3>Preencha os campos do(a) aluno(a) que irá usar a plataforma</h3>
+        <form className="formulario">
+          <div className="input-duplo">
+            <TextField label="Nome" required />
+            <TextField label="Sobrenome" required />
+          </div>
+          <div className="input-single">
+            <TextField label="Email" style={{ width: "100%" }} required />
+          </div>
+          <div className="input-single">
+            <TextField
+              label="Confirmar email"
+              style={{ width: "100%" }}
+              required
+            />
+          </div>
+          <div className="input-single">
+            <TextField label="CPF" style={{ width: "100%" }} required />
+          </div>
+          <div className="input-single">
+            <TextField label="Senha" style={{ width: "100%" }} required />
+          </div>
+          <div className="input-single">
+            <TextField
+              label="Confirmar senha"
+              style={{ width: "100%" }}
+              required
+            />
+          </div>
+          <div className="input-duplo">
+            <TextField label="Data de nascimento" required />
+            <TextField label="Lattes ID" required />
+          </div>
+          <div className="input-single">
+            <TextField label="E-mail alternativo" style={{ width: "100%" }} />
+          </div>
+          <div className="botao-row">
+            <Button
+              sx={{
+                width: "138px",
+                height: "55px",
+              }}
+              variant="outlined"
+            >
+              Voltar
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "#6C63FF",
+                width: "208px",
+                height: "55px",
+              }}
+              variant="contained"
+            >
+              Entrar
+            </Button>
+          </div>
+        </form>
+      </section>
+      <aside className="aside">
+        <div className="background">
+          <Center>
+            <img src={imagem} style={{ width: "80%", height: "auto" }} />
+          </Center>
+        </div>
+      </aside>
+    </main>
+  );
 }
