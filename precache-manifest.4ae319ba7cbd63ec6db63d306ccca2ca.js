35ce8c6c66d25317a79ed5a2b7a0f57a98bfc17d
Matheus Rigolão <mrigolao@gmail.com>
2022-10-16 23:42:13 -0300

    Updates

---
+++ precache-manifest.4ae319ba7cbd63ec6db63d306ccca2ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce13ec77113097a5be68",
+    "url": "/Desafio_4_Etapa_Frontend/static/css/main.7365bd4b.chunk.css"
+  },
+  {
+    "revision": "ce13ec77113097a5be68",
+    "url": "/Desafio_4_Etapa_Frontend/static/js/main.ce13ec77.chunk.js"
+  },
+  {
+    "revision": "2dc1741203b5f4c8a5b5",
+    "url": "/Desafio_4_Etapa_Frontend/static/js/1.2dc17412.chunk.js"
+  },
+  {
+    "revision": "67232f39f19f4e5dc522",
+    "url": "/Desafio_4_Etapa_Frontend/static/js/runtime~main.67232f39.js"
+  },
+  {
+    "revision": "4bd9e7bff20c58f5e27b3d16657dd574",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/planeta.4bd9e7bf.svg"
+  },
+  {
+    "revision": "2e456a0c62b9d3a4b6034967ed5b686d",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/community 1.2e456a0c.svg"
+  },
+  {
+    "revision": "3b7a190e3e90cca6519fb407b4677703",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/logo_fisica.3b7a190e.svg"
+  },
+  {
+    "revision": "0dc9d9637d9be4dae285dbf15c62f786",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/logo_matematica.0dc9d963.svg"
+  },
+  {
+    "revision": "15f6013f0a9ab7ef4c4d16a769e0689c",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/logo_quimic.15f6013f.svg"
+  },
+  {
+    "revision": "d89d3d7ad63898e9161a64a84c751f93",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/logo_IA.d89d3d7a.svg"
+  },
+  {
+    "revision": "bb4763b1c8d8fbf8bc5b6096dda215b3",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/logo_portugues.bb4763b1.svg"
+  },
+  {
+    "revision": "38772242e959621116f31752a32f88f9",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/logo_economia.38772242.svg"
+  },
+  {
+    "revision": "b82517b9168ad7ab4e1aa1f4edb6841c",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/pessoa1.b82517b9.svg"
+  },
+  {
+    "revision": "71961d4334b0213a8911912b0d8c268c",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/team 1.71961d43.svg"
+  },
+  {
+    "revision": "e7dd2e92e6524b9258c5f94578e27fe5",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/cadastro.e7dd2e92.svg"
+  },
+  {
+    "revision": "5295aadcca6e04bcfabe3377adba00de",
+    "url": "/Desafio_4_Etapa_Frontend/static/media/cientista.5295aadc.svg"
+  },
+  {
+    "revision": "8b3a8b09f6254854e81bfea5fc05f195",
+    "url": "/Desafio_4_Etapa_Frontend/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Desafio_4_Etapa_Frontend/precache-manifest.4ae319ba7cbd63ec6db63d306ccca2ca.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Desafio_4_Etapa_Frontend/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
