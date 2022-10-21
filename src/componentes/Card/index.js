b43d738caf8e5331f9c466ab3afe27359dcc8bb7
Joao Marques <joamarquescontato@gmail.com>
2022-10-21 01:13:38 -0300

    Componente card estático, card-avatar

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -5,20 +5,29 @@ import "./Card.css";
 const Card = (props) => {
   return (
     <div className="card-container">
-      <div className="card">
-        <div className="card-bg"></div>
-        <div className="card-conteudo">
-          <div className="card-avatar">
-            <img src="https://github.com/JoaaoM.png" />
-          </div>
-          <h4>Matemática Aplicada</h4>
-          <p>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old.
-          </p>
-          <Button variant="contained">Ler Mais</Button>
+      <div className="card-top">
+        <div className="card-imagem-bg">
+          <img src="https://github.com/JoaaoM.png" />
         </div>
+      </div>
+
+      <div className="card-bottom">
+        <h3>João Marques</h3>
+        <div className="card-info-container">
+          <div className="card-info">
+            <h4>Desenvolvedor</h4>
+            <span>Area Atuação</span>
+          </div>
+          <div className="card-info">
+            <h4>Desenvolvedor</h4>
+            <span>Area Formação</span>
+          </div>
+        </div>
+        <p>
+          Minhas habilidades são em front end, trabalhando com frameworks como
+          React, PrimeFaces
+        </p>
+        <Button variant="outlined">Perfil</Button>
       </div>
     </div>
   );
